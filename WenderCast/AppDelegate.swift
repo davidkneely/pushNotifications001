53c66f4ba0565ce7a13b26155e849661115e13e3
davidkneely <davidics613@gmail.com>
2018-03-12 09:20:54 -1000

    Sets up the notificaitons to be used once the app confirms that the permissions are set and allow for push notificaitons

--- WenderCast/AppDelegate.swift
+++ WenderCast/AppDelegate.swift
@@ -54,12 +54,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
             (granted, error) in
             print("Permission granted: \(granted)")
+            
+            guard granted else {return}
+            self.getNotificationSettings()
         }
     }
     
     func getNotificationSettings() {
         UNUserNotificationCenter.current().getNotificationSettings { (settings) in
             print("Notification settings: \(settings)")
+            guard settings.authorizationStatus == .authorized else {return}
+            UIApplication.shared.registerForRemoteNotifications()
         }
     }
 }
